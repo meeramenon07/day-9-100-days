3607e2b7d5631a6581be93e1056c0e6264360777
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 14:01:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Generation Identifier")
+name = input("What is Your Name? : ")
+yearOfBirth = int(input("Which year were you born? : "))
+if yearOfBirth >= 1883 and yearOfBirth <= 1900 :
+  print("Ohh! Lost Generation! Great Writers and reckless riches!")
+elif yearOfBirth >= 1901 and yearOfBirth <= 1927 :
+  print("Wow! Greatest Generation! World War II, Great Depression, Sacrifices and more!")
+elif yearOfBirth >= 1928 and yearOfBirth <= 1945:
+  print("Oops! Silent Generation!Obedient, hard-working!")
+elif yearOfBirth >= 1946 and yearOfBirth <= 1964:
+  print("Wow! Baby Boomers!Strong work ethics, competitive, self-assured, goal-oriented!")
+elif yearOfBirth >= 1965 and yearOfBirth <= 1980:
+  print("Hah! Generation X!Self-Reliance, Work-Life Balance, Financial Pressure and more!")
+elif yearOfBirth >= 1981 and yearOfBirth <= 1996:
+  print("Wow! Millennials! Generation Me, Tech savvy and more!")
+elif yearOfBirth >= 1997 and yearOfBirth <= 2012:
+  print("Hah! Generation Z! Zoomers, Tech savvy, best educated generation!")
+else:
+  print("Generation Alpha! Hyper-connected, Online always, 3D everything, ipad, facial recognition and so much more!")
+
+
+
+
+
